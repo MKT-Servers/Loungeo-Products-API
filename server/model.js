af7704e4fa6ea5e38cfe1d05f517ca144387790b
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-20 13:56:41 -0800

    serve received db code

--- server/model.js
+++ server/model.js
@@ -3,7 +3,21 @@ const db = require('../db/index.js');
 const get = (req, res, callback) => {
   // db.connect();
 // console.log("hello server");
-  res.send(db.connect());
+  res.send(db.connect(
+
+    (err, client, done) => {
+      if (err) throw err;
+      client.query('SELECT * FROM product WHERE id = $1', [9], (err, res) => {
+        done();
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log(res.rows[0]);
+        }
+      });
+    })
+
+  );
 
   // db.query(
   //   `SELECT * FROM groceries`,
