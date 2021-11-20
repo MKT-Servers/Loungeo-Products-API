45ac59b94f40336c6b1932d2bad21f790aea8b4b
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-20 13:55:34 -0800

    Stripped db to just creating the connection and moved everything else to model

--- db/index.js
+++ db/index.js
@@ -1,24 +1,6 @@
 const { Pool } = require('pg');
 const config = require('./config.js');
 
-const pool = {};
-
-pool.connect = () => {
-  console.log('Hello from db');
-};
-
-// const pool = new Pool(config);
-
-// pool.connect ((err, client, done) => {
-//   if (err) throw err;
-//   client.query('SELECT * FROM product WHERE id = $1', [9], (err, res) => {
-//     done();
-//     if (err) {
-//       console.log(err.stack);
-//     } else {
-//       console.log(res.rows[0]);
-//     }
-//   });
-// });
+const pool = new Pool(config);
 
 module.exports = pool;
