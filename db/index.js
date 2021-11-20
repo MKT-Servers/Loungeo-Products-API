d1ac681606cdc84bf8d9f52a50429c0d2c2d101f
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-20 13:47:40 -0800

    db talks to model

--- db/index.js
+++ db/index.js
@@ -1,19 +1,24 @@
 const { Pool } = require('pg');
 const config = require('./config.js');
 
-const pool = new Pool(config);
+const pool = {};
 
+pool.connect = () => {
+  console.log('Hello from db');
+};
 
-pool.connect ((err, client, done) => {
-  if (err) throw err;
-  client.query('SELECT * FROM product WHERE id = $1', [9], (err, res) => {
-    done();
-    if (err) {
-      console.log(err.stack);
-    } else {
-      console.log(res.rows[0]);
-    }
-  });
-});
+// const pool = new Pool(config);
 
-module.exports = connection;
+// pool.connect ((err, client, done) => {
+//   if (err) throw err;
+//   client.query('SELECT * FROM product WHERE id = $1', [9], (err, res) => {
+//     done();
+//     if (err) {
+//       console.log(err.stack);
+//     } else {
+//       console.log(res.rows[0]);
+//     }
+//   });
+// });
+
+module.exports = pool;
