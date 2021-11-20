6b408748b5ff62744d1d2d77cadc9148545bf385
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-19 20:58:23 -0800

    Same functionality as before, but added more pseudocode to prep for tomorrow

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const pgp = require('pg-promise')(/* options */);
+// const pgp = require('pg-promise')(/* options */);
 
 // const connection = {
 //   host: 'localhost',
